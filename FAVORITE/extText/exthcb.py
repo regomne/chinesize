8f0541541cc54314e8279fbfd6a7bca61a8de75d
regomne <fallingsunz@gmail.com>
2015-07-25 18:29:15 +0800

    [*]favo update

--- FAVORITE/extText/exthcb.py
+++ FAVORITE/extText/exthcb.py
@@ -22,31 +22,34 @@ fnt={
     0x1260:u'青空',
     0x13db:u'さやか',
     0x14ee:u'静',
-    0x1641:u'女１',
-    0x1687:u'担任教師',
-    0x16dd:u'女の子',
-    0x1756:u'ハク',
-    0x182e:u'レン',
-    0x1906:u'看護師',
-    0x1983:u'祖母',
-    0x1a20:u'お母さん',
-    0x1a99:u'お母さん',
-    0x1b10:u'かみさま',
-    0x1bbd:u'男の人',
-    0x1c36:u'レン',
-    0x1ca1:u'ユウマ',
-    0x1dd4:u'悠馬',
+    0x1641:u'苺',
+    0x1719:u'女１',
+    0x175f:u'担任教師',
+    0x17b5:u'女の子',
+    0x182e:u'ハク',
+    0x1906:u'レン',
+    0x19de:u'看護師',
+    0x1a5b:u'祖母',
+    0x1af8:u'お母さん',
+    0x1b71:u'お母さん',
+    0x1be8:u'かみさま',
+    0x1c95:u'男の人',
+    0x1d0e:u'レン',
+    0x1d79:u'ユウマ',
+    0x1eac:u'悠馬',
     
     }
 
-fs=open(ur'hikari.hcb','rb')
+fs=open(ur'shinku.hcb','rb')
 stm=bytefile.ByteIO(fs.read())
 parser=hcbParser.HcbParser(stm)
 
+
+
 parser.SetFuncNameTable(fnt,'932')
-##txt=parser.Parse()
-##fs=open('world1.txt','wb')
-##fs.write(txt.decode('932').encode('U16'))
-##fs.close
+#txt=parser.Parse()
+#fs=open('world1.txt','wb')
+#fs.write(txt.decode('932').encode('U16'))
+#fs.close()
 os.chdir('txt')
-parser.ParseTxt([(3587,3591),(3751,3867)],4171)
+parser.ParseTxt([(1439,4423),(4484,4770)],5042)
