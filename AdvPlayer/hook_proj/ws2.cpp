3eeff801f88343efbaf6aa7d204724b30c335809
regomne <fallingsunz@gmail.com>
2017-08-01 22:45:13 +0800

    ws2 pack support

--- AdvPlayer/hook_proj/ws2.cpp
+++ AdvPlayer/hook_proj/ws2.cpp
@@ -36,7 +36,7 @@ void HOOKFUNC MyOpenFile1(wchar_t* arcName, wchar_t* entName) {
     if (itr != g_MyFiles.end()) {
         if (wcslen(arcName) >= 6) {
             wcscpy(arcName, itr->second.c_str());
-//             Log(L"Pic replaced: %s", entName);
+            //             Log(L"Pic replaced: %s", entName);
         }
         else {
             Log(L"Replace arc name error:%s\n", arcName);
@@ -60,14 +60,14 @@ template<class T>
 int bin_search(const T* start, uint32_t cnt, const T& val) {
     auto init_start = start;
     auto end = start + cnt;
-    
+
     while (start <= end)
     {
         auto middle = start + (end - start) / 2;
         if (*middle > val) {
             end = middle - 1;
         }
-        else if(*middle < val){
+        else if (*middle < val) {
             start = middle + 1;
         }
         else {
@@ -108,7 +108,7 @@ uint32_t HOOKFUNC MyReadInst(Registers* regs, uint32_t old_proc_func) {
     auto scriptEnd = (uint8_t*)scriptObj[9];
     auto curOff = scriptObj[4];
     auto scriptSize = scriptEnd - scriptStart;
-    
+
     static TextInfo* txtInfo = nullptr;
     if (g_Ws2HasChanged) {
         g_Ws2HasChanged = false;
@@ -204,50 +204,6 @@ void HOOKFUNC MyChangeFont(Registers* regs) {
     regs->ecx = (DWORD)font_name;
 }
 
-vector<wstring> GetFileList(wchar_t* path) {
-    WIN32_FIND_DATA wfd;
-    vector<wstring> files;
-    auto hf = FindFirstFile(path, &wfd);
-    if (hf != INVALID_HANDLE_VALUE) {
-        do {
-            files.push_back(wfd.cFileName);
-        } while (FindNextFile(hf, &wfd));
-        FindClose(hf);
-    }
-    else {
-        Log("findFile fail");
-    }
-    return std::move(files);
-}
-
-vector<string>* ReadTxt(const wstring& txtName) {
-    MyFileReader reader;
-    auto mem = reader.ReadToMem(txtName.c_str());
-    if (!mem.Get()) {
-        return nullptr;
-    }
-    auto ansi = CvtToAnsi(mem, CP_UTF8);
-    auto lines = SplitTxtA(ansi);
-    while (lines[lines.size() - 1] == "")
-    {
-        lines.pop_back();
-    }
-    return new vector<string>(std::move(lines));
-}
-
-vector<uint32_t>* ReadIdx(const wstring& idxName, uint32_t* crc) {
-    MyFileReader reader;
-    auto mem = reader.ReadToMem(idxName.c_str());
-    if (!mem.Get()) {
-        return nullptr;
-    }
-    auto buff = (uint32_t*)mem.Get();
-    *crc = *buff;
-    auto vec = new vector<uint32_t>((mem.GetSize() - 4) / 4);
-    memcpy(&(*vec)[0], buff + 1, mem.GetSize() - 4);
-    return vec;
-}
-
 struct ArcHeader {
     uint32_t cnt;
     uint32_t idxSize;
@@ -258,7 +214,7 @@ struct EntryInfo {
     uint32_t off;
 };
 
-bool ReadArcFileList(const wchar_t* arcName, map<wstring,wstring>& fileMap) {
+bool ReadArcFileList(const wchar_t* arcName, map<wstring, wstring>& fileMap) {
     FILE* fp = nullptr;
     _wfopen_s(&fp, arcName, L"rb");
     if (!fp) {
@@ -270,7 +226,7 @@ bool ReadArcFileList(const wchar_t* arcName, map<wstring,wstring>& fileMap) {
     for (uint32_t i = 0;i < hdr.cnt;i++) {
         EntryInfo info;
         fread(&info, 1, sizeof(info), fp);
-        
+
         wstringstream s;
         wchar_t c;
         fread(&c, 1, sizeof(c), fp);
@@ -287,18 +243,97 @@ bool ReadArcFileList(const wchar_t* arcName, map<wstring,wstring>& fileMap) {
     return true;
 }
 
+#pragma pack(1)
+struct FileEntry {
+    char fname[51];
+    uint32_t offset;
+    uint32_t size;
+};
+#pragma pack()
+
+vector<wstring> GetFileList(wchar_t* path) {
+    WIN32_FIND_DATA wfd;
+    vector<wstring> files;
+    auto hf = FindFirstFile(path, &wfd);
+    if (hf != INVALID_HANDLE_VALUE) {
+        do {
+            files.push_back(wfd.cFileName);
+        } while (FindNextFile(hf, &wfd));
+        FindClose(hf);
+    }
+    else {
+        Log("findFile fail");
+    }
+    return std::move(files);
+}
+
+#include "key.h"
+
+void XorDec(void* dst, uint32_t len) {
+    auto p = (uint8_t*)dst;
+    for (uint32_t i = 0;i < len;i++) {
+        p[i] ^= g_dat_key[i & 1023];
+    }
+}
+
+map<wstring, FileEntry> g_fileInfos;
+
+vector<wstring> GetFileList(NakedMemory& mem) {
+    auto bf = (uint8_t*)mem.Get();
+    auto file_cnt = *(uint32_t*)bf;
+    auto entries = (FileEntry*)(bf + 4);
+    vector<wstring> files;
+    for (int i = 0;i < file_cnt;i++) {
+        files.push_back(decode_string(entries[i].fname, CP_UTF8));
+        g_fileInfos[files[i]] = entries[i];
+    }
+    return std::move(files);
+}
+
+vector<string>* ReadTxt(NakedMemory& pack, const wstring& txtName) {
+    auto& entry = g_fileInfos[txtName];
+    NakedMemory mem((uint8_t*)pack.Get() + entry.offset, entry.size);
+    auto ansi = CvtToAnsi(mem, CP_UTF8);
+    auto lines = SplitTxtA(ansi);
+    while (lines[lines.size() - 1] == "")
+    {
+        lines.pop_back();
+    }
+    return new vector<string>(std::move(lines));
+}
+
+vector<uint32_t>* ReadIdx(NakedMemory& pack, const wstring& idxName, uint32_t* crc) {
+    auto& entry = g_fileInfos[idxName];
+    NakedMemory mem((uint8_t*)pack.Get() + entry.offset, entry.size);
+    auto buff = (uint32_t*)mem.Get();
+    *crc = *buff;
+    auto vec = new vector<uint32_t>((mem.GetSize() - 4) / 4);
+    memcpy(&(*vec)[0], buff + 1, mem.GetSize() - 4);
+    return vec;
+}
+
 void InitWs2() {
-    auto txtList = GetFileList(L"txt\\*.txt");
+    MyFileReader reader;
+    auto mem = reader.ReadToMem("cnpack");
+    if (!mem.Get()) {
+        Log(L"Can't read pack.");
+        return;
+    }
+    XorDec(mem.Get(), mem.GetSize());
+    auto txtList = GetFileList(mem);
     for (auto& txtName : txtList) {
-        auto idxFullName = L"idx\\" + txtName.substr(0, txtName.length() - 4) + L".idx";
-        auto txtFullName = L"txt\\" + txtName;
-        auto lines = ReadTxt(txtFullName);
+        if (txtName.substr(txtName.length() - 3) != L"txt") {
+            continue;
+        }
+        auto idxFullName = L"idx\\" + txtName.substr(4, txtName.length() - 8) + L".idx";
+        auto txtFullName = txtName;
+        auto lines = ReadTxt(mem, txtFullName);
         if (!lines) {
             Log(L"Reading %s error.", txtFullName.c_str());
             continue;
         }
         uint32_t crc;
-        auto idxs = ReadIdx(idxFullName, &crc);
+        auto idxs = ReadIdx(mem, idxFullName, &crc);
         if (!idxs) {
             Log(L"Reading %s eror.", idxFullName.c_str());
             delete lines;
