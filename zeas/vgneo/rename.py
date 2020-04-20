908335763e2deb2e468cdfa173745b7c980a2952
regomne <fallingsunz@gmail.com>
2020-04-20 16:24:16 +0800

    delete some unusable files

--- zeas/vgneo/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-path1='fini'
-path2='final'
-
-for f in os.listdir(path1):
-    if f.endswith('.txt.out.bin'):
-        shutil.move(os.path.join(path1,f),os.path.join(path2,f.replace('.txt.out.bin','')))
--- zeas/想瞬譜～そうしゅんふ～/modhdr.py
+++
@@ -1,4 +0,0 @@
-import os,sys
-
-data = 'MjPlainBytecode\0'+open(sys.argv[1],'rb').read()[16:]
-open(sys.argv[1],'wb').write(data)
